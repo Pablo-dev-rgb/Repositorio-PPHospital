99dcc9ab74a090480e0b2f9bfbb4d2723dae157a
Pablo-dev.rgb <villabeba777gmail.com>
2025-04-16 17:59:37 -0300

    Rutas y Layout

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,12 +2,31 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 
+import LayoutPublic from "./Layouts/LayoutPublic";
+import LayoutAdmin from "./Layouts/LayoutAdmin";
+import LayoutClient from "./Layouts/LayoutClient";
+
+import ProtectedRoutes from "./pageauth/ProtectedRoutes";
+import PageHome from "./pagePublic/PageHome";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 const App = () => {
     return(
-        <div>
-            App
-            <button className="btn btn-primary">BOTON</button>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<LayoutPublic/>}>
+                    <Route index element={<PageHome/>}/>
+                </Route>
+            <Route element={<ProtectedRoutes/>}>
+                <Route path="/admin" element={<LayoutAdmin/>}>
+                    <Route index element={<PageHome/>} />
+                </Route>
+                <Route path="/client" element={<LayoutClient/>}>
+                    <Route index element={<PageHome/>} />
+                </Route>
+            </Route>
+            </Routes>
+        </Router>
     )
 }
 
